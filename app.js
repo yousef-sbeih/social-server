f414c21c489fce069136e6965dd414a400ce12fa
yousef sbeih <yousefjsbeih@gmail.com>
2022-11-29 02:26:02 +0300

    m

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 console.log(process.env.MONGODB_URI);
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/socialDB",
-  {}
+  { useNewUrlParser: true, useUnifiedTopology: true }
 );
 app.use(cors());
 const User = require("./models/user");
