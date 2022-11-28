5d64de0d12d36f43c2aabdbf4c09f9f3bfc547f2
yousef sbeih <yousefjsbeih@gmail.com>
2022-11-29 02:14:57 +0300

    add bcjs

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { deleteFromArray } = require("../helper");
 const jwt = require("jsonwebtoken");
 router.get("/", async (req, res) => {
